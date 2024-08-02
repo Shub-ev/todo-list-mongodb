4e490752d1559c7108997b62bf781cd44a6e524c
Shub-ev <shubhamg200204@gmail.com>
2024-08-02 13:40:35 +0530

    a

--- logic/signup.js
+++ logic/signup.js
@@ -1,47 +1,73 @@
-const name = document.getElementById("name");
+const nameIn = document.getElementById("name");
 const pass1 = document.getElementById("pass1");
 const pass2 = document.getElementById("pass2");
 const signup = document.getElementById("signup");
 const login = document.getElementById("login");
 const form = document.querySelector("form");
 
-form.onsubmit = (eve) => {
-    eve.preventDefault();
+const pass = [pass1, pass2];
+
+pass.map((pas) => {
+    pas.addEventListener('click', () => {
+        pas.style.border = "1px solid grey";
+        pas.setAttribute("placeholder", "");
+    });
+})
+
+function err(element) {
+    element.style.borderColor = "red";
+    element.value = "";
+    element.setAttribute("placeholder", (element == pass1 ? "Password size minimum 8" : "password should be same"));
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
     const p1 = pass1.value;
     const p2 = pass2.value;
-    const nm = name.value;
+    const nm = nameIn.value;
 
+    if(p1.length < 8){
+        err(pass1);
+        return;
+    }
     if (p1 !== p2) {
-        pass2.style.borderColor = "red";
-        pass2.value = "";
-        pass2.setAttribute("placeholder", "Passwords did not match");
+        err(pass2);
         return;
     }
 
-    const data = {
+    const credentials = {
         name: nm,
         pass: p1,
     }
 
     const url = "http://localhost:1324/";
 
-    fetch(url, {
+    const fetchPromise = fetch(url, {
         method: "POST",
         mode: 'cors',
-        body: JSON.stringify(data),
+        body: JSON.stringify(credentials),
         headers: {
             "Content-Type": "application/json",
         },
     })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error(error);
-        })
+    
+    fetchPromise.then((response) => {
+        const res = document.createElement("p");
+        if (!response.ok) {
+            res.textContent = res.body;
+            console.log(res);
+            document.appendChild(res);
+            throw new Error('Network response was not ok');
+        }else{
+            const jsonPromise = response.json();
+            jsonPromise.then((result) => {
+                console.log(result);
+                
+            })
+        }
+    }).catch(error => {
+        console.error(error);
+    })
 }
 
 signup.addEventListener('click', () => {
--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-const http = require("http");
-const port = 1324;
-const host = "localhost";
+const http = require('http');  // import http module
+const mongodb = require('mongodb'); // importing mongodb driver
+const port = 1324;  // setting the port number on which the server will respond
+const host = "localhost";  // domain for the the server will be localhost
+const uri = process.env['URI_MONGO'];
 
-const server = http.createServer((req, res) => {
-    const headers = {
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Methods': "OPTIONS, POST, GET, DELETE",
+console.log(uri);
+
+
+const serverReq = (req, res) => {   // creating a function (call back) for createServer() method of http module which acceps 2 paras
+    // 1. req which is the request object 
+    // 2. res response object
+    const header = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
         'Access-Control-Allow-Headers': 'Content-Type',
     }
-    if(req.method === "POST"){
+    if (req.method === "POST") {     // checking if req is of POST method
         let body = '';
 
-        req.on('data', chunk => {
+        req.on('data', chunk => {   // collecting data of request into single string
             body += chunk;
         })
         req.on('end', () => {
             const data = JSON.parse(body);
             console.log(data);
         })
-    }
 
-    res.writeHead(200, headers);
-    res.end();
-});
+        res.writeHead(200, header);
+        res.end(JSON.stringify({ res: "Okay" }));
+    }
+    else {
+        res.writeHead(204, header);
+        res.end(JSON.stringify({ res: "Not okay" }));
+    }
+}
+
+const server = http.createServer(serverReq); // createServer is the method of the 
 
 server.listen(port, host, () => {
-    console.log("Server runnung at port 1324");
+    console.log(`Server is running at port:${port}`);
 })
